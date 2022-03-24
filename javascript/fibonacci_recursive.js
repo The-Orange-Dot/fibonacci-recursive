257d56c78da58f0b1da669e533c614a71f3f1f2f
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-24 12:10:15 +0900

    Completed

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,6 +1,15 @@
 function fibonacci(n) {
-  // type your code here
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+/** Call-Stack
+ * fib(3) => fib(2) + fib(1) => 1 + 1 = 2
+ * fib(4) => fib(3) + fib(2) => 2 + 1 = 3
+ * fib(5) => fib(4) + fib(3) => 3 + 2 = 5
+ */
 
 if (require.main === module) {
   // add your own tests in here
@@ -22,3 +31,10 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/**
+ * if n < 2, just return n
+ * recurse should have fibonacci(arr, n)
+ *
+ * base case should be if arr.length >= n return arr[arr.length - n]
+ */
